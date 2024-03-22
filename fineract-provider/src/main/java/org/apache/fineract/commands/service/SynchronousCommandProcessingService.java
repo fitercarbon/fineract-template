88c467d6404fa180dbadc3fe62714ea03b95feea
Deepika1095 <57247520+Deepika1095@users.noreply.github.com>
2024-03-22 19:03:56 +0530

    FSCAR-10 Publish connected webhook event with one action. https://fiterio.atlassian.net/browse/FSCAR-10

--- fineract-provider/src/main/java/org/apache/fineract/commands/service/SynchronousCommandProcessingService.java
+++ fineract-provider/src/main/java/org/apache/fineract/commands/service/SynchronousCommandProcessingService.java
@@ -276,4 +276,62 @@ public class SynchronousCommandProcessingService implements CommandProcessingSer
         }
     }
 
+    // This specially designed for internal actions which is called during other events.
+    // EX Deposit internally call during premature.
+    @Override
+    public void publishEventInternalForActions(final String entityName, final String actionName, JsonCommand command, final Object result) {
+        Gson gson = new Gson();
+        try {
+            final AppUser appUser = this.context.authenticatedUser(CommandWrapper.wrap(actionName, entityName, null, null));
+
+            final HookEventSource hookEventSource = new HookEventSource(entityName, actionName);
+
+            // TODO: Add support for publishing array events
+            if (command.json() != null && command.json().startsWith("{")) {
+                Type type = new TypeToken<Map<String, Object>>() {
+
+                }.getType();
+                Map<String, Object> myMap = gson.fromJson(command.json(), type);
+
+                Map<String, Object> reqmap = new HashMap<>();
+                reqmap.put("entityName", entityName);
+                reqmap.put("actionName", actionName);
+                reqmap.put("createdBy", context.authenticatedUser().getId());
+                reqmap.put("createdByName", context.authenticatedUser().getUsername());
+                reqmap.put("createdByFullName", context.authenticatedUser().getDisplayName());
+
+                reqmap.put("request", myMap);
+                if (result instanceof CommandProcessingResult) {
+                    CommandProcessingResult resultCopy = CommandProcessingResult
+                            .fromCommandProcessingResult((CommandProcessingResult) result);
+
+                    reqmap.put("officeId", resultCopy.getOfficeId());
+                    reqmap.put("clientId", resultCopy.getClientId());
+                    resultCopy.setOfficeId(null);
+                    reqmap.put("response", resultCopy);
+                } else if (result instanceof ErrorInfo) {
+                    ErrorInfo ex = (ErrorInfo) result;
+                    reqmap.put("status", "Exception");
+
+                    Map<String, Object> errorMap = gson.fromJson(ex.getMessage(), type);
+                    errorMap.put("errorCode", ex.getErrorCode());
+                    errorMap.put("statusCode", ex.getStatusCode());
+
+                    reqmap.put("response", errorMap);
+                }
+
+                reqmap.put("timestamp", Instant.now().toString());
+
+                final String serializedResult = this.toApiResultJsonSerializer.serialize(reqmap);
+
+                final HookEvent applicationEvent = new HookEvent(hookEventSource, serializedResult, appUser,
+                        ThreadLocalContextUtil.getContext());
+
+                applicationContext.publishEvent(applicationEvent);
+            }
+        } catch (Exception e) {
+            log.error("Error", e);
+        }
+    }
+
 }
